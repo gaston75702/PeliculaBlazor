293ab631600fb3ea6bf2f8ab3198fdc508f75036
gaston pellegrini <gastonpellegrini298@outlook.com>
2023-01-19 14:53:44 -0300

    Biografia_no_funciona_css

---
+++ Shared/Entidades/Actor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeliculaBlazor.Shared.Entidades
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; } = null!;
+        public string? Biografia { get; set; }
+        public string? Foto { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+
+    }
+
+}
