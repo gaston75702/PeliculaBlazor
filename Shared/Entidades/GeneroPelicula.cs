1168c4fbb9e3b1c5e3b48f7735543da9e375542f
gaston pellegrini <gastonpellegrini298@outlook.com>
2023-01-24 20:42:40 -0300

    Seleccion_generos

---
+++ Shared/Entidades/GeneroPelicula.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeliculaBlazor.Shared.Entidades
+{
+    public class GeneroPelicula
+    {
+        public int PeliculaId {get; set;}
+        public int GeneroId { get; set; }
+    }   
+}
--- Shared/Entidades/Pelicula.cs
+++ Shared/Entidades/Pelicula.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,17 @@ namespace PeliculaBlazor.Shared.Entidades
 {
     public class Pelicula
     {
-        public string Titulo { get; set; }
-        public DateTime Lanzamiento { get; set; }
-        public string Poster { get; set; }
-        public string TituloCortado
+        public int Id { get; set; }
+        [Required]
+        public string Titulo { get; set; } = null!;
+        public string? Resumen { get; set; }
+        public bool EnCartelera { get; set; }
+        public string? Trailer { get; set; }
+
+        public DateTime? Lanzamiento { get; set; }
+        public string? Poster { get; set; } = null!;
+        public List<GeneroPelicula> GenerosPelicula { get; set; } = new List<GeneroPelicula>();
+        public string? TituloCortado
         {
             get
             {
