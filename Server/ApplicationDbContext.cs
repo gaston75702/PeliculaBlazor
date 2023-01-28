1c91ec8f981f63fa166db62bd9cc0a3ddd75e694
gaston pellegrini <gastonpellegrini298@outlook.com>
2023-01-28 12:55:21 -0300

    clase_program_server_esta_distinta

---
+++ Server/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PeliculaBlazor.Server
+{
+    public class ApplicationDbContext : DbContext
+    { 
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
--- Shared/Entidades/Actor.cs
+++ Shared/Entidades/Actor.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,21 @@ namespace PeliculaBlazor.Shared.Entidades
         public string? Biografia { get; set; }
         public string? Foto { get; set; }
         public DateTime? FechaNacimiento { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Actor a2)
+            {
+                return Id == a2.Id;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
 
     }
 
