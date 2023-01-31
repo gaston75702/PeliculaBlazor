ab823f123f66f3c1a2acb93051d73d9a75749476
gaston pellegrini <gastonpellegrini298@outlook.com>
2023-01-31 13:51:19 -0300

    Discrepancia_en_clases_program_server

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +26,7 @@ namespace PeliculaBlazor.Server
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+            services.AddDbContext<ApplicationDbContext>(opciones=>opciones.UseSqlServer("name=DefaultConnection"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
